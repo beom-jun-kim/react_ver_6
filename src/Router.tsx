b5440c42b4bc5b95b996fe3ed5c222e4866f9783
kimbeomjun <gimb52043@gmail.com>
2023-06-16 16:31:55 +0900

    error element / createBrowerRouter

---
+++ src/Router.tsx
@@ -0,0 +1,36 @@
+import Home from "./router/Home";
+import About from "./router/About";
+import Header from "./components/Header";
+import App from "./App";
+import { createBrowserRouter } from "react-router-dom";
+import NotFound from "./router/NotFound";
+
+// createBrowserRouter: Router를 array 형식으로 표현할 수 있게 해준다
+const router = createBrowserRouter([
+  {
+    // "/" : 모두의 부모
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        // "" : Home 컴포넌트 이동
+        path: "",
+        element: <Home />,
+      },
+      {
+        // "경로이름" : app을 render (outlet을 render)
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "header",
+        element: <Header />,
+      },
+    ],
+
+    // 404에러
+    errorElement: <NotFound />,
+  },
+]);
+
+export default router;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return <div>
+    <ul>
+      <li><Link to={"/"}>Home</Link></li>
+      <li><Link to={"/about"}>about</Link></li>
+    </ul>
+  </div>;
+}
+
+export default Header;
