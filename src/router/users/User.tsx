4051c89c2b881294284fb16082c1095f3a32831f
kimbeomjun <gimb52043@gmail.com>
2023-06-20 12:43:50 +0900

    주석 수정

--- src/router/users/User.tsx
+++ src/router/users/User.tsx
@@ -5,7 +5,7 @@ import { Link, Outlet } from "react-router-dom";
 function User() {
 
   //  Route path와 일치하는 현재 URL에서 동적 매개변수의 key/value 쌍 객체를 반환. 
-  // 하위 경로는 상위 경로에서 모든 매개변수를 상속
+  // 하위 경로는 상위 경로에서 모든 매개변수를 상속한다
   const { userId } = useParams();
   return (
     <div>
